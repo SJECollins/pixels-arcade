6c91fae3d8645603631d84faf90c25d57053922c
SJECollins <sje.collins@gmail.com>
2024-01-01 18:03:18 +0000

    Add theme toggle - unreasonably large commit
    -- Add script to toggle between alt and default themes on every HTML file
    -- Update root variables and all CSS files to account for toggling
    -- Unreasonble number of modified files...
    -- May have forgotten to update new game Codebreaker, but will check that when properly added to site

---
+++ assets/scripts/theme.js
@@ -0,0 +1,49 @@
+const toggleBtn = document.getElementById("toggle-display")
+let initialTheme = true;
+
+const retrieveTheme = () => {
+    const theme = localStorage.getItem("theme")
+    if (theme == "alt") {
+        initialTheme = false
+        setColors()
+    } else {
+        initialTheme = true
+        setColors()
+    } 
+}
+
+const toggleColors = () => {
+    const theme = localStorage.getItem("theme")
+    if (theme == "alt") {
+        localStorage.setItem("theme", "default")
+        initialTheme = true
+        setColors()
+    } else {
+        localStorage.setItem("theme", "alt")
+        initialTheme = false
+        setColors()
+    }
+}
+
+const setColors = () => {   
+  const root = document.documentElement;
+  console.log("clicked")
+  console.log(initialTheme)
+
+  if (initialTheme) {
+    console.log("change to default")
+    root.style.setProperty("--primary-text", "#282828")
+    root.style.setProperty("--secondary-text", "#25C40D")
+    root.style.setProperty("--primary-background", "#EEF1DB")
+    root.style.setProperty("--secondary-background", "#282828")
+  } else {
+    console.log("change to alt")
+    root.style.setProperty("--primary-text", "#25C40D")
+    root.style.setProperty("--secondary-text", "#282828")
+    root.style.setProperty("--primary-background", "#282828")
+    root.style.setProperty("--secondary-background", "#EEF1DB")
+  }
+}
+
+document.addEventListener("DOMContentLoaded", retrieveTheme)
+toggleBtn.addEventListener("click", toggleColors)
