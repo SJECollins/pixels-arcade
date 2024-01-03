f9071960164395d57f43b1e236b416082a5b8840
SJECollins <sje.collins@gmail.com>
2024-01-03 19:58:07 +0000

    Spawn geese and player

--- assets/scripts/goose.js
+++ assets/scripts/goose.js
@@ -25,7 +25,7 @@ window.addEventListener("load", () => {
     let bulletArray = []
     let gooseTimer = 0
     let bulletTimer = 0
-    let gooseInterval = 1000
+    let gooseInterval = 3000
     let bulletInterval = 0
     let randomGooseInterval = Math.random() * 1000 + 1000
     let lastTime = 0
@@ -60,10 +60,18 @@ window.addEventListener("load", () => {
 
     class Player {
         constructor() {
-
+            this.width = tileSize
+            this.height = tileSize * 2
+            this.x = 144
+            this.y = gameHeight - this.height
+            this.img = gunImg
+            this.frameX = 64
+            this.frameY = 0
+            this.facing = "up"
+            this.shooting = false
         }
         draw(context) {
-
+            context.drawImage(this.img, this.frameX, this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
         }
         shoot() {
 
@@ -91,42 +99,124 @@ window.addEventListener("load", () => {
     }
 
     class Goose {
-        constructor() {
-
+        constructor(spawnX, imgY, movingTo) {
+            this.width = tileSize
+            this.height = tileSize
+            this.x = spawnX
+            this.y = 224
+            this.img = gooseImg
+            this.frameX = 0
+            this.frameY = imgY
+            this.fps = 10
+            this.frameCount = 1
+            this.frameTimer = 0
+            this.frameInterval = 2000/this.fps
+            this.speed = 0.2
+            this.direction = movingTo
+            this.shot = false
+            this.markedforDeletion = false
         }
         draw(context) {
-
+            if (!this.markedforDeletion) {
+                context.drawImage(this.img, this.frameX * this.width, this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
+            }
         }
-        update() {
+        update(deltaTime) {
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.frameCount) {
+                    this.frameX = 0
+                } else {
+                    this.frameX++
+                }
+                this.frameTimer = 0
+            } else {
+                this.frameTimer += deltaTime
+            }
 
+            // Moving
+            if (!this.shot) {
+                if (this.direction == "right") {
+                    this.x += this.speed
+                    this.y -= this.speed
+                } else if (this.direction == "left") {
+                    this.x -= this.speed
+                    this.y -= this.speed
+                } else {
+                    this.y -= this.speed
+                }                
+            } else {
+                this.frameY = 96
+                this.y += this.speed
+            }
+            // Delete the goose
+            if (this.y > gameHeight) {
+                this.markedforDeletion = true
+            }
         }
     }
 
-    const handleGeese = () => {
-
+    const handleGeese = (deltaTime) => {
+        let spawnX = 0
+        let imgY = 0
+        let movingTo = ""
+        console.log("Goose timer: ", gooseTimer)
+        console.log("Goose interval: ", gooseInterval)
+        console.log("Random goose interval: ", randomGooseInterval)
+        if (gooseTimer > gooseInterval + randomGooseInterval) {
+            spawnX = Math.floor(Math.random() * 9) * 32
+            if (spawnX < 96) {
+                imgY = 32
+                movingTo = "right"
+            } else if (spawnX > 192) {
+                imgY = 64
+                movingTo = "left"
+            } else {
+                imgY = 0
+                movingTo = "up"
+            }
+            const newGoose = new Goose(spawnX, imgY, movingTo)
+            gooseArray.push(newGoose)
+            randomGooseInterval = Math.random() * 4000 + 2500
+            gooseTimer = 0
+            console.log("goose spawed")
+        } else {
+            gooseTimer += deltaTime
+        }
+        gooseArray.forEach(goose => {
+            goose.draw(ctx)
+            goose.update(deltaTime)
+        })
+        gooseArray = gooseArray.filter(goose => !goose.markedforDeletion)
     }
 
     const background = new Background()
     const leftTree = new Foreground(0, 32, treeImg, 0, 64, 192)
     const rightTree = new Foreground(gameWidth - 64, 32, treeImg, 64, 64, 192)
     const grass = new Foreground(0, gameHeight - 128, grassImg, 0, gameWidth, 128)
+    const player = new Player()
+
+    const startGame = () => {
+        startBtn.removeEventListener("click", startGame)
+        runGame(0)
+    }
 
     const runGame = (timeStamp) => {
         const deltaTime = timeStamp - lastTime
         lastTime = timeStamp
         ctx.clearRect(0, 0, gameWidth, gameHeight)
         background.draw(ctx)
-
+        handleGeese(deltaTime)
         leftTree.draw(ctx)
         rightTree.draw(ctx)
         grass.draw(ctx)
+        player.draw(ctx)
 
         if (!gameOver) {
             requestAnimationFrame(runGame)
         }
     }
 
-    startBtn.addEventListener("click", runGame)
+    startBtn.addEventListener("click", startGame)
     resetBtn.addEventListener("click", () => {
         location.reload()
     })
