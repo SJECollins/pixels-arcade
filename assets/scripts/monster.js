678be84d1dc3025f22f9f7af4d14a1aebd671172
SJECollins <sje.collins@gmail.com>
2024-01-23 12:37:07 +0000

    Trigger battle scene

--- assets/scripts/monster.js
+++ assets/scripts/monster.js
@@ -8,6 +8,8 @@ window.addEventListener("load", () => {
     // Images
     const bgImg = new Image()
     bgImg.src = "../assets/images/monster/maptwo.png"
+    const battleBgImg = new Image()
+    battleBgImg.src = "../assets/images/monster/battlebg.png"
     const houseImg = new Image()
     houseImg.src = "../assets/images/monster/housetwo.png"
     const labImg = new Image()
@@ -16,18 +18,26 @@ window.addEventListener("load", () => {
     waterImg.src = "../assets/images/monster/watertwo.png"
     const postImg = new Image()
     postImg.src = "../assets/images/monster/posttwo.png"
+    const wallImg = new Image()
+    wallImg.src = "../assets/images/monster/bigposttwo.png"
     const signImg = new Image()
     signImg.src = "../assets/images/monster/signtwo.png"
     const grassImg = new Image()
-    grassImg.src = "../assets/images/monster/grass.png"
+    grassImg.src = "../assets/images/monster/grasssheet.png"
     const playerImg = new Image()
     playerImg.src = "../assets/images/monster/trainer.png"
+    const playerBtlImg = new Image()
+    playerBtlImg.src = "../assets/images/monster/playerbtl.png"
+    const snartleImg = new Image()
+    snartleImg.src = "../assets/images/monster/fakesquirtle_sprite.png"
+
 
     const gameWidth = 320
     const gameHeight = 288
     const tileSize = 16
     const obstacleArray = []
     const grassArray = []
+    let battle = false
     let lastTime = 0
 
     // Map
@@ -55,6 +65,70 @@ window.addEventListener("load", () => {
         }
         draw(context) {
             context.drawImage(this.img, this.x, this.y, this.width, this.height)
+        }
+    }
+
+    class Grass {
+        constructor(posX, posY) {
+            this.img = grassImg
+            this.x = posX
+            this.y = posY
+            this.width = tileSize
+            this.height = tileSize
+            this.moving = false
+            this.frameX = 0
+            this.frameY = 0
+            this.frameCount = 1
+            this.fps = 6
+            this.frameTimer = 0
+            this.frameInterval = 1000/this.fps
+        }
+        draw(context) {
+            context.drawImage(this.img, this.frameX * this.width, this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
+        }
+        update(deltaTime) {
+            if (this.x < player.x + player.width &&
+                this.x + this.width > player.x &&
+                this.y < player.y + player.height &&
+                this.y + this.height > player.y) {
+                this.moving = true
+            } else {
+                this.moving = false
+            }
+            if (this.moving) {
+                if (this.frameTimer > this.frameInterval) {
+                    if (this.frameX >= this.frameCount) {
+                        this.frameX = 0
+                    } else {
+                        this.frameX++
+                    }
+                    this.frameTimer = 0
+                } else {
+                    this.frameTimer += deltaTime
+                }
+            } else {
+                this.frameX = 0
+            }
+        }
+        triggerBattle() {
+            if (this.moving) {
+                let battleChance = Math.floor(Math.random() * 1000)
+                if (battleChance == 1) {
+                    battle = true
+                    battleScene()
+                }
+            }
+        }
+    }
+
+    class Message {
+        constructor(lineOne) {
+            this.lineOne = lineOne
+        }
+        draw(ctx) {
+            ctx.font = "20px sans-serif"
+            ctx.fillStyle = "#081820"
+            ctx.fillText(this.lineOne, 32, 228)
         }
     }
 
@@ -99,7 +173,7 @@ window.addEventListener("load", () => {
         update(input) {
             this.x += this.moveX
             this.y += this.moveY
-            if (input.keys.indexOf("KeyD") > -1 && this.x < gameWidth - 32) {
+            if (input.keys.indexOf("KeyD") > -1) {
                 if (this.movingRight && obstacleArray.some(checkCollisions)) {
                     this.moveX = 0
                     this.moving = false
@@ -113,7 +187,7 @@ window.addEventListener("load", () => {
                 this.movingLeft = false
                 this.movingDown = false            
                 }
-            } else if (input.keys.indexOf("KeyA") > -1 && this.x > 16) {
+            } else if (input.keys.indexOf("KeyA") > -1) {
                 if (this.movingLeft && obstacleArray.some(checkCollisions)) {
                     this.moveX = 0
                 } else {
@@ -126,7 +200,7 @@ window.addEventListener("load", () => {
                 this.movingLeft = true
                 this.movingDown = false 
                 }
-            } else if (input.keys.indexOf("KeyW") > -1 && this.y > 32) {
+            } else if (input.keys.indexOf("KeyW") > -1) {
                 if (this.movingUp && obstacleArray.some(checkCollisions)) {
                     this.moveY = 0
                 } else {
@@ -139,7 +213,7 @@ window.addEventListener("load", () => {
                 this.movingLeft = false
                 this.movingDown = false 
                 }
-            } else if (input.keys.indexOf("KeyS") > -1 && this.y < gameHeight - 32) {
+            } else if (input.keys.indexOf("KeyS") > -1) {
                 if (this.movingDown && obstacleArray.some(checkCollisions)) {
                     this.moveY = 0
                 } else {
@@ -161,7 +235,6 @@ window.addEventListener("load", () => {
     }
 
     const checkCollisions = (obstacle) => {
-
         if (obstacle.img == houseImg || obstacle.img == labImg) {
             if (player.x < obstacle.x + obstacle.width && 
                 player.x + player.width > obstacle.x && 
@@ -206,20 +279,11 @@ window.addEventListener("load", () => {
         }
     }
 
-    // const handleInputs = (e) => {
-    //     if (e.code == "KeyA") {
-    //         player.update(e.code)
-    //     } else if (e.code == "KeyD") {
-    //         player.update(e.code)
-    //     } else if (e.code == "KeyS") {
-    //         player.update(e.code)
-    //     } else if (e.code == "KeyW") {
-    //         player.update(e.code)
-    //     }
-    // }
 
-    const createObjects = () => {
-        // Create Buildings
+    const firstScene = () => {
+        background.img = bgImg
+        obstacleArray.length = 0
+        grassArray.length = 0
         const firstHouse = new StaticObject(houseImg, 64, 48, 64, 48)
         obstacleArray.push(firstHouse)
         const secondHouse = new StaticObject(houseImg, 192, 48, 64, 48)
@@ -248,40 +312,91 @@ window.addEventListener("load", () => {
         obstacleArray.push(secondPost)
         for (let i = 0; i < 2; i++) {
             for (let j = 0; j < 2; j++) {
-                const grass = new StaticObject(grassImg, 160 + (j * 16), 0 + (i * 16), tileSize, tileSize)
-                obstacleArray.push(grass)
+                const grass = new Grass(160 + (j * 16), 0 + (i * 16))
+                grassArray.push(grass)
             }
         }
+        for (let i = 0; i < 17; i++) {
+            const wall = new StaticObject(wallImg, 0, 16 + (16 * i), tileSize, tileSize)
+            obstacleArray.push(wall)
+            const otherWall = new StaticObject(wallImg, 304, 16 + (16 * i), tileSize, tileSize)
+            obstacleArray.push(otherWall)
+        }
+        for (let i = 0; i < 9; i++) {
+            const wall = new StaticObject(wallImg, 16 + (16 * i), 16, tileSize, tileSize)
+            obstacleArray.push(wall)
+        }
+        for (let i = 0; i < 7; i++) {
+            const wall = new StaticObject(wallImg, 192 + (16 * i), 16, tileSize, tileSize)
+            obstacleArray.push(wall)
+        }
+        for (let i = 0; i < 3; i++) {
+            const wall = new StaticObject(wallImg, 16 + (16 * i), 272, tileSize, tileSize)
+            obstacleArray.push(wall)
+        }
+        for (let i = 0; i < 11; i++) {
+            const wall = new StaticObject(wallImg, 128 + (16 * i), 272, tileSize, tileSize)
+            obstacleArray.push(wall)
+        }
+    }
+
+    const battleScene = () => {
+        background.img = battleBgImg
+        obstacleArray.length = 0
+        grassArray.length = 0
+        const playerBattle = new StaticObject(playerBtlImg, 32, 80, 100, 100)
+        obstacleArray.push(playerBattle)
+        const pokemonBattle = new StaticObject(snartleImg, 172, 32, 100, 100)
+        obstacleArray.push(pokemonBattle)
+        displayMessage.lineOne = "A wild Snartle appeared!"
+    }
+
+    const throwBall = () => {
+
     }
 
     const handleObjects = (ctx) => {
         obstacleArray.forEach(object => object.draw(ctx))
     }
 
+    const handleGrass = (ctx, deltaTime) => {
+        grassArray.forEach(grass => {
+            grass.draw(ctx)
+            grass.update(deltaTime)
+            grass.triggerBattle()
+        })
+    }
 
     const startGame = () => {
-        // window.addEventListener("keydown", handleInputs)
-        createObjects()
+        firstScene()
         runGame()
     }
 
     const background = new Background()
     const player = new Player()
     const input = new InputHandler()
+    const displayMessage = new Message()
 
     function runGame(timeStamp) {
         const deltaTime = timeStamp - lastTime
         lastTime = timeStamp
         ctx.clearRect(0, 0, gameWidth, gameHeight)
         background.draw(ctx)
-        
-        player.draw(ctx, deltaTime)
-        player.update(input)
+        handleGrass(ctx, deltaTime)
+        if (!battle) {
+            player.draw(ctx, deltaTime)
+            player.update(input)            
+        }
+        if (battle) {
+            displayMessage.draw(ctx)
+        }
         handleObjects(ctx)
         requestAnimationFrame(runGame)
 
     }
 
     startBtn.addEventListener("click", startGame)
-
+    resetBtn.addEventListener("click", () => {
+        location.reload()
+    })
 })
