898a9b282b6561b427ff0d68dad4a78dc20413a7
SJECollins <sje.collins@gmail.com>
2024-07-31 17:42:58 +0000

    Start word guessing game

---
+++ assets/scripts/guessword.js
@@ -0,0 +1,130 @@
+const wordGrid = document.getElementById("word-grid")
+const guessInput = document.getElementById("guess-input")
+
+const startBtn = document.getElementById("start")
+const resetBtn = document.getElementById("reset")
+const guessBtn = document.getElementById("guess-button")
+
+const gameVars = {
+    "length": 0,
+    "guesses": [],
+    "word": ""
+}
+
+const preventNumeric = (e) => {
+    e.target.value = e.target.value.replace(/[^a-zA-Z]/g, '')
+}
+
+const getLength = () => {
+    return parseInt(document.querySelector("input[name='word-length']:checked").value)
+}
+
+const renderGrid = (length) => {
+    wordGrid.innerHTML = ""
+
+    wordGrid.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+    wordGrid.style.gridTemplateRows = `repeat(${length}, 1fr)`;
+
+    const totalCells = length * length;
+    for (let i = 1; i <= totalCells; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add("grid-cell", "cell")
+        wordGrid.appendChild(gridCell);
+    }
+
+    for (let i = 1; i <= length; i++) {
+        const guessCell = document.createElement('input')
+        guessCell.type = "text"
+        guessCell.name = "guess"
+        guessCell.maxLength = 1
+        guessCell.classList.add("cell", "guess-cell")
+        guessCell.addEventListener("input", preventNumeric)
+        guessInput.appendChild(guessCell)
+    }
+}
+
+const pickWord = (length) => {
+    if (length == 3) {
+        return threeLetters[Math.floor(Math.random() * threeLetters.length)]
+    } else if (length == 4) {
+        return fourLetters[Math.floor(Math.random() * fourLetters.length)]
+    } else {
+        return fiveLetters[Math.floor(Math.random() * fiveLetters.length)]
+    }
+}
+
+// const checkLetter = (guess) => {
+//     for (let i = 0; i < guess.length; i++) {
+//         if (gameVars["word"][i] == guess[i]) {
+//             return correctPlace
+//         } else if (gameVars["word"].includes(guess[i])) {
+//             return correctLetter
+//         } else {
+//             return wrong
+//         }
+//     }
+// }
+
+const checkGuess = () => {
+    let guess = []
+    let correct = 0
+    
+    const guessCells = document.querySelectorAll(".guess-cell")    
+    guessCells.forEach(cell => guess.push(cell.value))
+    console.log(guess)
+    console.log(gameVars["word"])
+
+    if (guess.length != gameVars["word"].length) {
+        return console.log("wrong length")
+    }
+
+    const gridCells = document.querySelectorAll(".grid-cell")
+    let emptyGridCells = Array.from(gridCells).filter(cell => cell.innerHTML === "");
+
+    for (let i = 0; i < guess.length; i++) {
+        const cell = emptyGridCells[i];
+        const guessedLetter = guess[i];
+        
+        if (gameVars["word"][i] === guessedLetter) {
+            cell.classList.add("correct-place");
+            correct++;
+        } else if (gameVars["word"].includes(guessedLetter)) {
+            cell.classList.add("correct-letter");
+        } else {
+            cell.classList.add("wrong-letter");
+        }
+        
+        cell.innerHTML = guessedLetter;
+    }
+
+
+    if (correct === gameVars["word"].length) {
+        console.log("Congratulations! You guessed the word!");
+    }
+
+    guessCells.forEach(cell => cell.value = "");
+}
+
+const startGame = () => {
+    startBtn.removeEventListener("click", startGame)
+    gameVars["length"] = getLength()
+    renderGrid(gameVars["length"])
+    gameVars["word"] = pickWord(gameVars["length"])
+    guessBtn.addEventListener("click", checkGuess)
+}
+
+const endGame = () => {
+
+}
+
+startBtn.addEventListener("click", startGame)
+resetBtn.addEventListener("click", () => {
+    location.reload()
+})
+
+
+const threeLetters = ["not", "net", "the", "yes", "ate", "new", "fin", "fit", "fat", "ten", "tic", "toc", "ton", "she", "her", "him", "hot", "his", "hat", "day", "son", "sin", "sun", "set", "sat", "sit", "sum", "sad", "men", "man", "mic", "mad"]
+
+const fourLetters = ["thin", "then", "than", "tone", "tune", "that", "they", "them", "sent", "send", "sand", "sign", "sing", "yell", "bang", "tell", "bent", "days", "week", "weak", "tall", "feel", "felt", "yelp", "sumo", "song", "sang"]
+
+const fiveLetters = ["their", "yells", "theme", "tunes", "tones", "tuner", "sings", "songs", "signs", "human", "catch", "canoe", "banjo", "bangs", "bingo", "bento", "weeks", "weird", "waste", "waist", "shame", "feels", "cause"]
